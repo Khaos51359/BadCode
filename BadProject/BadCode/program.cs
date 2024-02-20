272e0628d333bcb2737df73cb3e2e326f187082c
Khaos51359 <khaos51359@gmail.com>
2024-02-20 11:09:54 +0800

    test hello world dotnet build

---
+++ BadProject/BadCode/program.cs
@@ -0,0 +1,10 @@
+namespace BadCode
+{
+    public class BadCode
+    {
+        public static void Main()
+        {
+            System.Console.WriteLine("D4rkn1 starts coding");
+        }
+    }
+}
