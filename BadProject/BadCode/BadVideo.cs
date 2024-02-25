f6d61c5a25a562907154e6a42b61f044e5ef0a1c
Khaos51359 <khaos51359@gmail.com>
2024-02-25 08:31:02 +0800

    add video functions

--- BadProject/BadCode/BadVideo.cs
+++ BadProject/BadCode/BadVideo.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using OpenCvSharp;
 
@@ -6,19 +7,145 @@ namespace BadCode
 {
     public class BadVideo
     {
+        private  VideoCapture _badVideo;
+        private int _badVideoFrameCount;
+        private float _baDVideoFPS;
+        private int _badVideoWidth;
+        private int _badVideoHeight;
+        private int _badVideoLength;
+        public const float FRAME_CONVERT_PERCENTAGE_TREESHOLD = 0.3f;
+
         public BadVideo(string fileName)
         {
-            using (VideoCapture vid = new VideoCapture(fileName))
+            VideoCapture vid = new VideoCapture(fileName);
+
+            if(!vid.IsOpened())
             {
-                if(!vid.IsOpened())
-                {
-                    Console.WriteLine("[BadLog] cannot open file named" +
-                        fileName + " in " + Directory.GetCurrentDirectory()
-                    );
-                    return;
-                }
-                Console.WriteLine(vid.FrameWidth + "x" + vid.FrameHeight);
+                Console.WriteLine("[BadLog] cannot open " +
+                        Directory.GetCurrentDirectory() + fileName
+                        );
+                return;
             }
+
+            _badVideo = vid;
+            _badVideoFrameCount = (int)vid.Get(VideoCaptureProperties.FrameCount);
+            _baDVideoFPS = (int)vid.Get(VideoCaptureProperties.Fps);
+            _badVideoWidth = (int)vid.Get(VideoCaptureProperties.FrameWidth);
+            _badVideoHeight = (int)vid.Get(VideoCaptureProperties.FrameHeight);
+            _badVideoLength = (int)(_badVideoFrameCount / _baDVideoFPS);
+
         }
+
+        ~BadVideo()
+        {
+             _badVideo.Release();
+        }
+
+        public Mat[] GetBadVideoMats()
+        {
+            Mat[] allMats = new Mat[_badVideoFrameCount];
+
+            for(int i = 0; i < allMats.Length; i++)
+            {
+                allMats[i] = new Mat();
+                _badVideo.Read(allMats[i]);
+            }
+
+            _badVideo.Set(VideoCaptureProperties.PosFrames, 0);
+
+            return allMats;
+        }
+
+        public BadAppleFrame[] GetBadAppleFrames(int frameTime)
+        {
+            Mat[] mat = GetBadVideoMats();
+
+            BadAppleFrame[] badAppleFrames =
+                new BadAppleFrame[GetBadFrameCount(frameTime)];
+
+            for (int i = 0; i < badAppleFrames.Length; i++)
+            {
+                badAppleFrames[i] = ConvertRawFrame(mat[i]);
+            }
+
+            return badAppleFrames;
+        }
+
+        private BadAppleFrame ConvertRawFrame(Mat frame)
+        {
+            int badFrameWidth = BadAppleFrame.WIDTH;
+            int badFrameHeight = BadAppleFrame.HEIGHT;
+
+            short[,] badFrame = new short[badFrameWidth, badFrameHeight];
+
+            int totalPixelPerBadFrameWidth = (int)(_badVideoWidth / badFrameWidth);
+            int totalPixelPerBadFrameHeight = (int)(_badVideoHeight / badFrameHeight);
+            int totalPixelPerBadFrame = totalPixelPerBadFrameWidth +
+                totalPixelPerBadFrameHeight;
+
+            for (int i = 0; i < badFrameWidth; i++)
+            {
+                for (int j = 0; j < badFrameHeight; j++)
+                {
+                    int[] pixelAddress = new int[]{i,j};
+
+                    badFrame[i,j] = ConvertToBadPixel(pixelAddress, frame,
+                        totalPixelPerBadFrameWidth, totalPixelPerBadFrameHeight);
+                }
+            }
+
+            return new BadAppleFrame(badFrame);
+        }
+
+        private byte ConvertToBadPixel(int[] pixelAddress, Mat frame, int width,
+                int height)
+        {
+            int pixelColumnStart = pixelAddress[0] * width;
+            int pixelColumnEnd = pixelColumnStart + width;
+            int pixelRowStart = pixelAddress[1] * height;
+            int pixelRowEnd = pixelRowStart + height - 1;
+
+            int originalPixelPerBadFrame = width * height;
+            int whitePixelCounter = 0;
+
+            for (int i = pixelRowStart; i < pixelRowEnd ; i++)
+            {
+                for (int j = pixelColumnStart; j < pixelColumnEnd ; j++)
+                {
+                    if (frame.At<byte>(i,j) >= 100)
+                        whitePixelCounter++;
+                }
+            }
+
+            int pixelTreeshold =
+                (int)(FRAME_CONVERT_PERCENTAGE_TREESHOLD * originalPixelPerBadFrame);
+
+            bool isWhite = whitePixelCounter > pixelTreeshold;
+
+            return isWhite ? (byte)255:(byte)0;
+        }
+
+        private int GetBadFrameCount(int frameTime)
+        {
+            return _badVideoLength * 1000 / frameTime;
+        }
+
+
+        public int GetFrameCount()
+        {
+            return _badVideoFrameCount;
+        }
+
+        public int GetVideoLength()
+        {
+            return _badVideoLength;
+        }
+
+
+        public float GetFPS()
+        {
+            return _baDVideoFPS;
+        }
+
     }
 }
